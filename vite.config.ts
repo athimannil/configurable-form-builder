ff7061a7758263c85797c2fc3f0164a659b2779d
Muhammed Athimannil <hello@athimannil.com>
2026-02-21 19:19:07 +0100

    feat: Add testing setup (Vitest & React Testing Library)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
 
-// https://vite.dev/config/
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'src/setupTests.ts',
+        'dist/',
+        'build/',
+        '*.config.*',
+        'cypress/**',
+      ],
+    },
+  },
+});
